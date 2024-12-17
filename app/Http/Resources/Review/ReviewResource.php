a1dddbc12cdc50accfd47b0b4ed49c2dacea15c8
toka <tokahassan692@gmail.com>
2024-12-17 13:31:46 +0200

    return grade instead of stage in user review

--- app/Http/Resources/Review/ReviewResource.php
+++ app/Http/Resources/Review/ReviewResource.php
@@ -18,7 +18,7 @@ class ReviewResource extends JsonResource
             'user'        => [
                 'name' => $this->user->name,
                 'image'  => $this->user->getFirstMediaUrl('image'),
-                'stage'  => $this->user->stage->translations->firstWhere('locale', request()->get('lang', app()->getLocale()))->name ,
+                'grade'  => $this->user->grade->name ,
             ],
             'review'      => $this->review,
         ];
